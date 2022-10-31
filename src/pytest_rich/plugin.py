da06bc5dbc50472fc036480b5e06c2fd5d902a46
Josh <josh@joshthomas.dev>
2022-10-31 14:12:13 -0500

    switch from depreciated hook to new `hookimpl`

--- src/pytest_rich/plugin.py
+++ src/pytest_rich/plugin.py
@@ -12,10 +12,9 @@ def pytest_addoption(parser):
     parser.addoption("--rich", action="store_true", default=False)
 
 
-@pytest.mark.trylast
+@pytest.mark.hookimpl(trylast=True)
 def pytest_configure(config):
     if sys.stdout.isatty() and config.getoption("rich"):
-        # Get the standard terminal reporter plugin and replace it with ours
         standard_reporter = config.pluginmanager.getplugin("terminalreporter")
         config.pluginmanager.unregister(standard_reporter)
         config.pluginmanager.register(
