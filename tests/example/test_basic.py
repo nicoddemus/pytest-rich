64d6bbddb116fe709fc8099fced60458cb2fa1fa
Josh Thomas <josh@joshthomas.dev>
2022-10-31 14:39:14 -0500

    Remove dummy tests and add basic plugin test comparisons to built-in reporter (#46)

--- tests/example/test_basic.py
+++ tests/example/test_basic.py
@@ -9,8 +9,23 @@ def test_fail():
     assert False
 
 
-def test_error():
+def test_setup_error(nonexistent_fixture):
+    pass
+
+
+@pytest.fixture
+def teardown_error_fixture():
+    yield
     raise Exception("error")
+
+
+def test_teardown_error(teardown_error_fixture):
+    pass
+
+
+@pytest.mark.skip
+def test_skip_no_reason():
+    ...
 
 
 @pytest.mark.skip(reason="skipped")
@@ -18,11 +33,59 @@ def test_skip():
     ...
 
 
+@pytest.mark.skip("skipped")
+def test_skip_no_keyword():
+    ...
+
+
+def test_inline_skip():
+    pytest.skip("skipped")
+
+
+@pytest.mark.xfail
+def test_xfail_no_reason():
+    assert False
+
+
 @pytest.mark.xfail(reason="xfail")
 def test_xfail():
     assert False
 
 
+def test_inline_xfail():
+    pytest.xfail("xfail")
+
+
 @pytest.mark.xfail(reason="xpass")
 def test_xpass():
     assert True
+
+
+def test_nested_failure():
+    def inner():
+        assert False
+
+    inner()
+
+
+# These two tests raise an `AssertionError`, so they are commented out
+# for now. They will be uncommented once the `AssertionError` is
+# handled.
+# Related issue: https://github.com/nicoddemus/pytest-rich/issues/44
+
+# def test_doubly_nested_failures():
+#     def inner():
+#         def inner_inner():
+#             assert False
+#         inner_inner()
+#     inner()
+
+
+# def test_triply_nested_failures():
+#     def inner():
+#         def inner_inner():
+#             def inner_inner_inner():
+#                 assert False
+#             inner_inner_inner()
+#         inner_inner()
+#     inner()
--- tests/test_rich.py
+++ tests/test_rich.py
@@ -1,7 +1,29 @@
-import pytest
+def test_outcomes(pytester):
+    pytester.copy_example("test_basic.py")
 
-# time.sleep(1)
-@pytest.mark.parametrize("i", range(20))
-def test(i):
-    # time.sleep(0.01)
-    pass
+    outcomes = {
+        "passed": 2,
+        "skipped": 4,
+        "failed": 2,
+        "errors": 2,
+        "xpassed": 1,
+        "xfailed": 3,
+    }
+
+    without_rich = pytester.runpytest()
+    with_rich = pytester.runpytest("--rich")
+
+    without_rich.assert_outcomes(**outcomes) == with_rich.assert_outcomes(**outcomes)
+
+
+def test_collect_error(pytester):
+    pytester.makepyfile(
+        """
+    raise Exception("collect error")
+    """
+    )
+
+    without_rich = pytester.runpytest()
+    with_rich = pytester.runpytest("--rich")
+
+    without_rich.assert_outcomes(errors=1) == with_rich.assert_outcomes(errors=1)
