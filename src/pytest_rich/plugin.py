de1fdd8ce4045d36637b75e38dfd56b4cacf2f11
Josh Thomas <josh@joshthomas.dev>
2022-10-31 14:51:36 -0500

    remove extraneous `mark` (#48)

--- src/pytest_rich/plugin.py
+++ src/pytest_rich/plugin.py
@@ -12,7 +12,7 @@ def pytest_addoption(parser):
     parser.addoption("--rich", action="store_true", default=False)
 
 
-@pytest.mark.hookimpl(trylast=True)
+@pytest.hookimpl(trylast=True)
 def pytest_configure(config):
     if sys.stdout.isatty() and config.getoption("rich"):
         standard_reporter = config.pluginmanager.getplugin("terminalreporter")
